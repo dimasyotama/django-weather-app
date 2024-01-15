0ddd3d6effe4fc915002746a6e6a8ccf473848e7
dimasyotama <digaart06@gmail.com>
2024-01-15 09:53:16 +0700

    change into using .env

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -81,17 +81,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
-<<<<<<< HEAD
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT')
-=======
         'USER':config('DB_USER'),
         'PASSWORD':config('DB_PASSWORD'),
         'HOST':config('DB_HOST'),
         'PORT':config('DB_PORT')
->>>>>>> dev
     }
 }
 
