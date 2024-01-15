dbb448f62ae3fc79568e6db088b53cca811f4c58
dimasyotama <digaart06@gmail.com>
2024-01-15 09:51:48 +0700

    modify

--- weather/views.py
+++ weather/views.py
@@ -5,20 +5,41 @@ from .forms import CityForm
 from .models import city
 from django.views.generic.edit import DeleteView
 from django.urls import reverse_lazy
+from decouple import config
 
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=1be6324f259e2d5ad5e3f216c7627890'
-    err_msg = ''
-    message = ''
-    message_class = ''
+    """
+    Render the weather index page, allowing users to add and view weather data for different cities.
+
+    Parameters:
+        request (HttpRequest): The Django HttpRequest object.
+
+    Returns:
+        HttpResponse: The rendered weather index page.
+
+    Raises:
+        None
+
+    Usage:
+        This view function handles both GET and POST requests. For GET requests, it renders the weather
+        index page with a form for adding cities and a list of existing cities with their weather data.
+        For POST requests, it processes the submitted form, adds the city to the database if it doesn't
+        already exist, and updates the weather data for all cities.
+
+    """
+
+    new_city, url, token_key = None, '', config('TOKEN_KEY', '')
+    err_msg, message, message_class = '', '', ''
     if request.method == 'POST':
         form = CityForm(request.POST)
         if form.is_valid():
             new_city = form.cleaned_data['name']
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={new_city}&units=metric&appid={token_key}'
+            print("INI ",url)
             existing_city_count = city.objects.filter(name=new_city).count()
             if existing_city_count == 0:
-                r = requests.get(url.format(new_city)).json()
+                r = requests.get(url).json()
                 print(r)
                 if r['cod'] == 200:
                     form.save()
@@ -33,15 +54,14 @@ def index(request):
             message = 'City added successfully!'
             message_class = "alert-success"
     
-    print(err_msg)
     form = CityForm()
     cities = city.objects.all()
 
     weather_data = []
 
     for citi in cities:
-
-        r = requests.get(url.format(citi)).json()
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={citi}&units=metric&appid={token_key}'
+        r = requests.get(url).json()
         city_weather  = {
             'city' : citi.name,
             'temperature' : r['main']['temp'],
@@ -59,13 +79,65 @@ def index(request):
     }
 
     return render(request, 'weather/weather.html',context)
+
 def about(request) :
+    """
+    Render the about page.
+
+    Parameters:
+        request (HttpRequest): The Django HttpRequest object.
+
+    Returns:
+        HttpResponse: The rendered about page.
+
+    Raises:
+        None
+
+    Usage:
+        This view function handles GET requests and renders the about page.
+
+    """
+     
     return render(request,'weather/about.html')
 
     
 def delete_city(request, city_name):
+    """
+    Delete a city from the database.
+
+    Parameters:
+        request (HttpRequest): The Django HttpRequest object.
+        city_name (str): The name of the city to be deleted.
+
+    Returns:
+        HttpResponseRedirect: Redirects to the home page after deleting the city.
+
+    Raises:
+        DoesNotExist: If the specified city does not exist in the database.
+
+    Usage:
+        This view function handles GET requests and deletes the specified city from the database.
+        After deletion, it redirects the user to the home page.
+
+    """
     city.objects.get(name=city_name).delete()
     return redirect('home')
 
 def help(request):
+    """
+    Render the help page.
+
+    Parameters:
+        request (HttpRequest): The Django HttpRequest object.
+
+    Returns:
+        HttpResponse: The rendered help page.
+
+    Raises:
+        None
+
+    Usage:
+        This view function handles GET requests and renders the help page.
+
+    """
     return render(request,'weather/help.html')
--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1be6324f259e2d5ad5e3f216c7627890'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,11 +78,11 @@ WSGI_APPLICATION = 'weatherapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'djangoweather',
-        'USER':'dimasyoga',
-        'PASSWORD':'password',
-        'HOST':'127.0.0.1',
-        'PORT':'5432'
+        'NAME': config('DB_NAME'),
+        'USER':config('DB_USER'),
+        'PASSWORD':config('DB_PASSWORD'),
+        'HOST':config('DB_HOST'),
+        'PORT':config('DB_PORT')
     }
 }
 
